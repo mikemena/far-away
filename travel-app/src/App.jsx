f52b3e3a5f4f9f65a875d79d8d542ed51f32d985
tokyo <tokyolabs@protonmail.com>
2024-02-08 21:48:41 -0500

    wip form

--- travel-app/src/App.jsx
+++ travel-app/src/App.jsx
@@ -1,15 +1,83 @@
+import React, { useState } from 'react';
 import './App.css';
-// import Steps from './components/steps';
-import Counter from './components/counter/counter';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: true },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+
+function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(8);
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Form submitted');
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip</h3>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" checked={item.packed} />
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.description} - {item.quantity}
+      </span>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have x items on your list, and you already packed x (x%)</em>
+    </footer>
+  );
+}
 
 function App() {
   return (
-    <>
-      <div>
-        <Counter />
-        {/* <Steps /> */}
-      </div>
-    </>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
   );
 }
 
