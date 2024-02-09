b5f52fb253abca55b8b2b2a68bc9226946deae22
tokyo <tokyolabs@protonmail.com>
2024-02-09 08:42:12 -0500

    wip

--- travel-app/src/App.jsx
+++ travel-app/src/App.jsx
@@ -12,16 +12,25 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState('');
-  const [quantity, setQuantity] = useState(8);
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(event) {
     event.preventDefault();
     console.log('Form submitted');
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
